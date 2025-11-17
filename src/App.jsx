9745550e1da737fa8c56da30d667eb50ea1db29d
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-11-17 16:42:34 +0700

    belajar kedua

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
 import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Card />
+      <Student name="Spongebob" age={30} isStudent={true} />
       <Button />
       <Food />
       <Footer />
